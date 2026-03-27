e91f55e911d2aa32bd727be1d4384fd7ad0b7171
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-03-27 12:58:47 +0900

    タイトルタグを修正

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- <title>Recipe Diary - 404 Not Found</title> -->
   <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon" />
   <?php wp_head(); ?>
 </head>
