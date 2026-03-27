bd0aaa7062a251fa12d84577b67dad91c801d21a
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-03-27 12:58:34 +0900

    リンクを修正

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
   <footer>
     <ul class="footer-nav">
-      <li><a href="<?php echo esc_url(home_url('/')); ?>contact/">お問い合わせ</a></li>
-      <li><a href="<?php echo esc_url(home_url('/')); ?>privacy-policy/">プライバシーポリシー</a></li>
+      <li><a href="<?php echo esc_url(home_url('/contact/')); ?>">お問い合わせ</a></li>
+      <li><a href="<?php echo esc_url(home_url('/privacy-policy/')); ?>">プライバシーポリシー</a></li>
     </ul>
     <ul class="sns">
       <li><a href="#">Instagram</a></li>
       <li><a href="#">Twitter</a></li>
       <li><a href="#">Facebook</a></li>
     </ul>
-    <small class="copyright">&copy; 2021 Recipe Diary</small>
+    <small class="copyright">&copy; 2021 <?php echo bloginfo('name'); ?></small>
   </footer>
   <!-- <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script> -->
   <?php wp_footer(); ?>
