2fa9a0bb2732ad10dca4ca466e28c7b401f65393
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-03-30 12:59:12 +0900

    全体的にリファクタリング

--- template-parts/linkbutton.php
+++ template-parts/linkbutton.php
@@ -1,15 +1,26 @@
 <?php
+$slug = $args[0];
 $link = '';
-if (!$args[0] === 'recipe') {
-    $to = get_page_by_path($args[0], OBJECT, 'page');
-    $title = $to->post_title;
-    $slug = $to->post_name;
-    $link = home_url('/') . $slug;
+$btn_data = array();
+
+if ($slug === 'recipe') {
+    $btn_data = [
+        'title' => 'レシピ一覧',
+        'link' => get_post_type_archive_link($args[0]),
+    ];
+} elseif ($slug === 'home') {
+    $btn_data = [
+        'title' => 'ホーム',
+        'link' => home_url('/'),
+    ];
 } else {
-    $link = get_post_type_archive_link($args[0]);
-    $title = 'レシピ';
+    $to = get_page_by_path($args[0], OBJECT, 'page');
+    $slug = $to->post_name;
+    $btn_data = [
+        'title' => $to->post_title,
+        'link' =>     $link = home_url('/') . $slug,
+    ];
 }
 
-
 ?>
-<a class="button" href="<?php echo esc_url($link); ?>"><?php echo $title; ?>に戻る</a>
+<a class="button" href="<?php echo esc_url($btn_data['link']); ?>"><?php echo $btn_data['title']; ?>に戻る</a>
