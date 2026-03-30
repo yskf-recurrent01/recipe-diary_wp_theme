9657ae0727a05ef4b32159d7742c5dfc03334d7a
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-03-30 12:57:05 +0900

    レシピカテゴリアーカイブページを作成

---
+++ template-parts/recipelist.php
@@ -0,0 +1,30 @@
+<?php
+$the_query = $args[0];
+?>
+
+<li class="recipe-list-item">
+	<?php if (has_post_thumbnail()) : the_post_thumbnail('medium', array('class' => 'recipe-list-img')); ?>
+	<?php else: ?>
+		<img src="https://placehold.jp/bfbfbf/ffffff/600x750.jpg?text=No%20Photo" alt="" class="recipe-list-img" />
+	<?php endif; ?>
+	<div class="recipe-list-info">
+		<?php
+		// カスタム投稿のカテゴリを取得するときはget_the_terms()を使う
+		$cats = get_the_terms(get_the_ID(), 'recipe_category');
+		if ($cats && !is_wp_error($cats)):
+			$current_cat = '';
+			if (is_tax('recipe_category')) {
+				// レシピに登録されているカテゴリーの中から、現在のアーカイブページのカテゴリーとマッチするものだけ抽出して表示する
+				// use()...コールバック関数のスコープ外で定義された変数を利用する
+				$current_cat = array_find($cats, function ($obj) use ($the_query) {
+					if ($obj->slug === $the_query->slug) return $obj;
+				});
+			}
+		?>
+			<ul class="recipe-category">
+				<li><a href="<?php echo esc_url(home_url('/') . 'recipe_category/' . (empty($current_cat)) ? $cats[0]->slug : $current_cat->slug); ?>"><?php echo (empty($current_cat)) ? $cats[0]->name : $current_cat->name; ?></a></li>
+			</ul>
+		<?php endif; ?>
+		<h2 class="recipe-list-title"><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	</div>
+</li>
