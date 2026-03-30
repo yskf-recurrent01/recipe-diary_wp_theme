c8d5be0a65a42274b7326f84f1b1becd6787200a
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-03-30 11:19:36 +0900

    カスタム投稿アーカイブ用に調整

--- template-parts/linkbutton.php
+++ template-parts/linkbutton.php
@@ -1,6 +1,15 @@
-<?php 
-$to = get_page_by_path($args[0],OBJECT,'page');
-$title = $to->post_title;
-$slug = $to->post_name;
+<?php
+$link = '';
+if (!$args[0] === 'recipe') {
+    $to = get_page_by_path($args[0], OBJECT, 'page');
+    $title = $to->post_title;
+    $slug = $to->post_name;
+    $link = home_url('/') . $slug;
+} else {
+    $link = get_post_type_archive_link($args[0]);
+    $title = 'レシピ';
+}
+
+
 ?>
-<a class="button" href="<?php echo esc_url(home_url('/')).$slug ?>"><?php echo $title; ?>に戻る</a>
+<a class="button" href="<?php echo esc_url($link); ?>"><?php echo $title; ?>に戻る</a>
